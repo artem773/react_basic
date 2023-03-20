189b660f572d5f92efc2f0a24aa804aec313be01
artem <artem140742@gmail.com>
2023-03-20 22:19:17 +0200

    state components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 /* import logo from './logo.svg'; */
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 
 const Header = () => {
-  return <h2>Hello artem</h2>
+	return <h2>Hello artem</h2>
 }
 
 const Field = () => {
-  const holder = ['Enter here..', 'Log in here..'];
-  const logged = true;
-  const styledField = {
-    width: '300px',
-    fontSize: '15px',
-    border: '2px solid #ffe837',
-    borderRadius: '4px',
-  };
+	const holder = ['Enter here..', 'Log in here..'];
+	const logged = true;
+	const styledField = {
+		width: '300px',
+		fontSize: '15px',
+		border: '2px solid #ffe837',
+		borderRadius: '4px',
+	};
 
-  return <input
-    type="text"
-    style={styledField}
-    placeholder={logged ? holder[0] : holder[1]} />
+	return <input
+		type="text"
+		style={styledField}
+		placeholder={logged ? holder[0] : holder[1]} />
 }
 
 // class Field extends React.Component{ //создаєм  такой же компонент Field с помощью класса 
@@ -41,31 +42,50 @@ const Field = () => {
 // }
 
 const Btn = () => {
-  const text = 'Log in.';
-  const logged = true;
+	const text = 'Log in.';
+	const logged = true;
 
-  return <button>{logged ? 'Enter' : text}</button>
+	return <button>{logged ? 'Enter' : text}</button>
 }
 
-const WhoAmI = ({name, surname, link}) => {
-  return(
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href="{link}">my page</a>
-    </div>
-  )
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: 'click'
+		}
+	}
+
+	nextYear = () => {
+		console.log('+++');
+		this.setState(state => ({
+			years: state.years + 1
+		}));
+	}
+
+	render() {
+		const { name, surname, link } = this.props;
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+				<a href={link}>my page</a>
+			</div>
+		)
+	}
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Field />
-      <Btn />
-      <WhoAmI name = {() => {return "Artem"}} surname = "Liskovshchenko" link = "https://www.facebook.com/artem.liskovschenko"/>
-      <WhoAmI name = {() => {return "Yuna"}} surname = "Levchenko" link = "https://www.facebook.com/yuna.levchenko"/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Field />
+			<Btn />
+			<WhoAmI name="Artem" surname="Liskovshchenko" link="https://www.facebook.com/artem.liskovschenko" />
+			<WhoAmI name="Yuna" surname="Levchenko" link="https://www.facebook.com/yuna.levchenko" />
+		</div>
+	);
 }
 
 export { Header };
