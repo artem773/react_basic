acf6c179419d1beb3d955757fe18d1a40ce990b8
artem <artem140742@gmail.com>
2023-04-05 18:24:53 +0300

    restyle all proj

--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,15 @@
-/* import logo from './logo.svg'; */
-import React from 'react';
-import { Component } from 'react';
-import './App.css';
 
-const Header = () => {
-	return <h2>Hello artem</h2>
-}
-
-const Field = () => {
-	const holder = ['Enter here..', 'Log in here..'];
-	const logged = true;
-	const styledField = {
-		width: '300px',
-		fontSize: '15px',
-		border: '2px solid #ffe837',
-		borderRadius: '4px',
-	};
-
-	return <input
-		type="text"
-		style={styledField}
-		placeholder={logged ? holder[0] : holder[1]} />
-}
-
-// class Field extends React.Component{ //создаєм  такой же компонент Field с помощью класса 
-//   render(){
-//     const holder = ['Enter here..', 'Log in here..'];
-//     const logged = true;
-//     const styledField = {
-//       width: '300px',
-//       fontSize: '15px',
-//       border: '2px solid #ffe837',
-//       borderRadius: '4px',
-//     };
-
-//     return <input 
-//     type="text" 
-//     style={styledField} 
-//     placeholder={logged ? holder[0] : holder[1]} />
-//   }
-// }
-
-const Btn = () => {
-	const text = 'Log in.';
-	const logged = true;
-
-	return <button>{logged ? 'Enter' : text}</button>
-}
-
-class WhoAmI extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			years: 27,
-			text: 'click',
-			position: ''
-		}
-	}
-
-	nextYear = () => {
-		console.log('+++');
-		this.setState(state => ({
-			years: state.years + 1
-		}));
-	}
-
-	inputChanges = (e, color) => {
-		console.log(color);
-		this.setState({
-			position: e.target.value
-		})
-	}
-
-	render() {
-		const { name, surname, link } = this.props;
-		const { years, position } = this.state;
-		return (
-			<div>
-				<button onClick={this.nextYear}>{this.state.text}</button>
-				<h3>My name is {name},
-					surname - {surname},
-					age - {years},
-					position - {position}
-				</h3>
-				<a href={link}>my page</a>
-				<form>
-					<span>Put your position</span>
-					<input type="text" onChange={(e) => this.inputChanges(e, 'some color')} />
-				</form>
-			</div>
-		)
-	}
-}
+import './App.css'
+import WhoAmI from './components/whoAmI/WhoAmI'
 
 function App() {
-	return (
-		<div className="App">
-			<Header />
-			<Field />
-			<Btn />
-			<WhoAmI name="Artem" surname="Liskovshchenko" link="https://www.facebook.com/artem.liskovschenko" />
-			<WhoAmI name="Yuna" surname="Levchenko" link="https://www.facebook.com/yuna.levchenko" />
-		</div>
-	);
+
+  return (
+    <div className='App'>
+      <WhoAmI name='John' surname='Wall' link='link.com' />
+      <WhoAmI name="Bob" surname="Swan" link="link.com" />
+    </div>
+  )
 }
 
-export { Header };
-export default App;
+export default App
---
+++ src/components/whoAmI/WhoAmI.jsx
@@ -0,0 +1,51 @@
+import { Component } from "react";
+import '../whoAmI/whoAmI.css';
+
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            position: ''
+        }
+    }
+
+    prevYear = () => {
+        this.setState(state => ({
+            years: state.years - 1
+        }))
+    }
+    nextYear = () => {
+        this.setState(state => ({
+            years: state.years + 1
+        }))
+    }
+    changePisition = (e) => {
+        e.preventDefault();
+        this.setState({
+            position: e.target.value
+        })
+    }
+
+    render() {
+        const { name, surname, link } = this.props;
+        const { years, position } = this.state;
+        return (
+            <div className="card">
+                <h1>My name is {name}<br />
+                    my surname - {surname}<br />
+                    age - {years}<br />
+                    position - {position} </h1 >
+                <button onClick={this.prevYear}>prev</button>
+                <button onClick={this.nextYear}>next</button>
+                <a className="profile_link" href={link}>my profile</a>
+                <form>
+                    <span>Put position: </span>
+                    <input type="text" onChange={this.changePisition} />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default WhoAmI;
